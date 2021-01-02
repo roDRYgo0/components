a75d152b9bb3139e144ec67f3c164aef7a016e8a
Rodrigo Castillo <dev.rodrig@outlook.com>
2021-01-02 01:45:26 -0600

    :art: add validation to type

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -90,6 +90,11 @@ export default {
     type: {
       type: String,
       default: 'button',
+      validator: (value) => [
+        'button',
+        'submit',
+        'reset',
+      ].indexOf(value) >= 0,
     },
     tag: {
       type: String,
@@ -119,88 +124,4 @@ export default {
     };
   },
 };
-// export default {
-//   props: {
-//     title: {
-//       type: String,
-//       required: true,
-//     },
-//     size: {
-//       type: String,
-//       default: 'md',
-//     },
-//     color: {
-//       type: String,
-//       default: 'primary',
-//     },
-//     light: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     iconPosition: {
-//       type: String,
-//       default: 'left',
-//     },
-//     isLoading: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     disabled: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     type: {
-//       type: String,
-//       default: 'button',
-//     },
-//   },
-
-//   emits: ['click'],
-
-//   computed: {
-//     sizeClass() {
-//       switch (this.size) {
-//         case 'xs':
-//           return 'px-2.5 py-1.5 text-xs leading-4 rounded';
-//         case 'sm':
-//           return 'px-3 py-2 text-sm leading-4 rounded-md';
-//         case 'md':
-//           return 'px-4 py-2 text-sm leading-5 rounded-md';
-//         case 'lg':
-//           return 'px-4 py-2 text-base leading-6 rounded-md';
-//         case 'xl':
-//           return 'px-6 py-3 text-base leading-6 rounded-md';
-//         default:
-//           return '';
-//       }
-//     },
-
-//     colorClass() {
-//       switch (this.color) {
-//         case 'light':
-//           if (this.isLoading || this.disabled) {
-//             return 'border-gray-300 text-gray-700 bg-white';
-//           }
-//           return 'border-gray-300 text-gray-700 bg-white hover:text-gray-500 focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800';
-//         default:
-//           if (this.light) {
-//             if (this.isLoading || this.disabled) {
-//               return `border-transparent text-${this.color}-700 bg-${this.color}-100`;
-//             }
-//             return `border-transparent text-${this.color}-700 bg-${this.color}-100 hover:bg-${this.color}-50 focus:border-${this.color}-300 focus:shadow-outline-${this.color} active:bg-${this.color}-200`;
-//           }
-//           if (this.isLoading || this.disabled) {
-//             return `border-transparent text-white bg-${this.color}-600`;
-//           }
-//           return `border-transparent text-white bg-${this.color}-600 hover:bg-${this.color}-500 focus:border-${this.color}-700 focus:shadow-outline-${this.color} active:bg-${this.color}-700`;
-//       }
-//     },
-//   },
-
-//   methods: {
-//     handleClick(e) {
-//       this.$emit('click', e);
-//     },
-//   },
-// };
 </script>
